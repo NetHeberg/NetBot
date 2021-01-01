43bb6ce2faade2640d782ab1c4b61423ee22a83f
Jean Staffe <69088819+IceroDev@users.noreply.github.com>
2021-01-01 12:21:23 +0100

    Modification de playstore.js
    Ajout du code du pull request #2

--- commands/playstore.js
+++ commands/playstore.js
@@ -1,5 +1,52 @@
 exports.run = async (client, message, args) => {
   var Discord = require("discord.js");
   var gplay = require("google-play-scraper");
-  message.channel.send("Cette commande est actuellement en maintenance");
+  let recherche = args.join(" ");
+  if (!recherche) return message.reply("vous n'avez demandé aucune recherche.");
+  var attente = new Discord.MessageEmbed()
+    .setDescription(
+      "<a:loading:756916814121926759> Je traite votre demande, un instant s'il vous plaît."
+    )
+    .setColor("2f3136");
+
+  message.channel.send(attente).then((m) => {
+    gplay
+      .search({
+        term: args,
+        num: 1,
+      })
+      .then((obj) => {
+        gplay.app({ appId: obj[0].appId, lang: "fr" }).then(
+          (App) => {
+            m.edit(
+              new Discord.MessageEmbed()
+                .setDescription(
+                  "**Nom : **" +
+                    App.title +
+                    "\n **Développeur : **" +
+                    App.developer +
+                    "\n **Version : **" +
+                    App.version +
+                    "\n **Description : **" +
+                    App.description +
+                    "\n **Installations : **" +
+                    App.installs +
+                    "\n **Score : **" +
+                    App.score +
+                    "\n **Taille : **" +
+                    App.size
+                )
+                .setColor("2f3136")
+            );
+          },
+          () => {
+            m.edit(
+              new Discord.MessageEmbed()
+                .setDescription(args + " Application non trouvée")
+                .setColor("2f3136")
+            );
+          }
+        );
+      });
+  });
 };
