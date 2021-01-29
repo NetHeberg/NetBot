54868f7259dfc14a9da5dd803c4e75b55e541045
Jean Staffe <69088819+IceroDev@users.noreply.github.com>
2021-01-29 21:44:20 +0100

    Ajout de la commande n!docs
    Une commande simple qui permet de faire des recherches sur la documentation de discord.js

---
+++ commands/docs.js
@@ -0,0 +1,23 @@
+exports.run = (client, message, args) => {
+  var Discord = require("discord.js");
+  const axios = require("axios")
+  if(!args[0])return message.reply("vous ne m'avez pas donné de recherche à faire.")
+  axios
+  .get('https://djsdocs.sorta.moe/v2/embed?src=stable&q='+args.join("+"))
+  .then(response => {
+    if(response.data !==null){
+    var embed = new Discord.MessageEmbed()
+    .setTitle("Documentation pour "+args.join(" "))
+    .setDescription(response.data.description)
+    .setColor("#2296f3")
+    .setFooter("netheberg.fr")
+    .setThumbnail("https://discord.js.org/static/logo-square.png");
+    message.channel.send(embed)
+  }else{
+    message.channel.send("Je n'ai rien trouvé pour `"+args.join(" ")+"`")
+  }
+  }).catch(error =>{
+    message.channel.send("Oops, quelque chose s'est mal passé. Réessayez plus tard.")
+  })
+    
+};
