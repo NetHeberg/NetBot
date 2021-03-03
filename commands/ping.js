c3c9ffdf3ceeaa22c21edf41210849efd5d8e6fb
Jean Staffe <69088819+IceroDev@users.noreply.github.com>
2021-03-03 11:45:13 +0100

    Patch 01

--- commands/ping.js
+++ commands/ping.js
@@ -1,7 +1,6 @@
 exports.run = (client, message, args) => {
   var Discord = require("discord.js");
   const axios = require("axios")
-    message.delete();
     if(!args[0])return message.reply("vous ne m'avez pas donné d'ip à ping.")
     if(args[0].includes(":"))return message.reply("je ne supporte malheureusement pas encore le ping d'un port.")
     const embed = new Discord.MessageEmbed()
@@ -11,10 +10,10 @@ exports.run = (client, message, args) => {
       .setFooter("netheberg.fr");
       message.channel.send(embed).then(m =>{
        axios
-        .get("https://api03.netheberg.fr/"+args[0])
+        .get("https://api03.thehostingbot.xyz/"+args[0])
         .then((response) => {
           var data = response.data
-          if(!data.error){
+          if(!data.pingInfo.error){
             embed.setDescription("```"+data.pingInfo.output+"```")
             embed.addField("• __Résumé__ :",`• ping : ${data.pingInfo.time}ms\n• IP : ${data.pingInfo.numeric_host}`)
             embed.setColor("GREEN")
